fb2f692dd6db42ade5450ee7abbee3cbf7e696ec
rhkanrdl <emrdl7@gmail.com>
2019-12-02 15:55:10 +0900

    정리

--- js/common.js
+++ js/common.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
 
 
 
+    $(window).on("scroll", function(){
+
+        console.log($(".list-pasture tbody").offset().top);
+
+        if ($(window).scrollTop() > $(".list-pasture tbody").offset().top) {
+            $(".list-pasture table").addClass("fit");
+            $(".list-pasture thead th:nth-child(1)").css("width", $(".list-pasture tbody tr:first-child td:nth-child(1)").outerWidth());
+            $(".list-pasture thead th:nth-child(2)").css("width", $(".list-pasture tbody tr:first-child td:nth-child(2)").outerWidth());
+            $(".list-pasture thead th:nth-child(3)").css("width", $(".list-pasture tbody tr:first-child td:nth-child(3)").outerWidth());
+        } else {
+            $(".list-pasture table").removeClass("fit");
+        }
+    });
+    
+
+
 });
 
